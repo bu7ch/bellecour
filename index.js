d1b800c327183aab8f8d800f995723c1721b4d19
Rodolphe <dutchr@live.fr>
2022-09-26 11:12:46 +0200

    ajout de nouvelles urls (route) & app.use + install nodemon

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-
-app.get('/', (req, res) => {
+const router = require("./routes");
+app.use(express.json());
+app.get("/", (req, res) => {
   res.send("Welcome to the Express API");
-})
+});
+app.get("/bonjour/:name", (req, res) => {
+  res.send("bonjour " + req.params.name);
+});
+// DRY
+// app.get('/sports/tennis', (req, res)=>{
+// res.send("c'est de la terre battue")
+// })
+// app.get('/sports/football', (req, res)=>{
+// res.send("Vive le football !!!!")
+// })
 
+app.use("/sports", router);
 
 app.listen(port, console.log(`⚡️ [Server is running on port :${port}]`));
+
---
+++ routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+ const infos = []
+router.get('/tennis', (req, res) => {
+  res.send('Merci Roger !!')
+})
+router.get('/football', (req,res) => {
+  res.send('Encore la coupe du monde ?!')
+})
+
+router.post('/ajouter/:sport', (req, res) => {
+  infos.push(req.params.sport)
+  res.send(`la liste des sports : ${infos}`)
+})
+
+module.exports = router
